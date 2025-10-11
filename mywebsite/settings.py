4cbbdcb835faa6432f3ba94539156e96a8f0b892
musthafa6750 <musthafa6750@gmail.com>
2025-10-11 13:44:03 +0300

    Fix: about

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -53,9 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-  
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this line
 ]
 
 ROOT_URLCONF = 'mywebsite.urls'
